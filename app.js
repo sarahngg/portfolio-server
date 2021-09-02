e8ada43e20f07eaa53ca8b8ffae77ae02c2f05df
Sarah Ng <sarah.ng26@gmail.com>
2021-09-02 16:27:00 -0700

    used http request object to serve pages conditionally

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'content-type': 'text/html' /** also called MIME type */
-  })
-  res.write('<h1>A page served on every single request</h1><div>Made possible by setting <tt>\'content-type\': \'text/html\'</tt></div>')
-  console.log('Something hit the server')
+  const { url, method } = req;
+  console.log(`${method} ${url}`)
+  if (url === '/') {
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write('<h1>Home</h1><div>Made possible by setting <tt>\'content-type\': \'text/html\'</tt></div>')
+  } else if (url === '/contact') {
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write('<h1>Contact</h1>')
+  } else {
+    res.writeHead(404, {'content-type': 'text/html'})
+    res.write('<h1>Page not found :(</h1>')
+  }
   res.end()
 })
 
