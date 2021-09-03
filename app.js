00b3ebff8e41b944207725e5eb5bd9bfc10e6467
Sarah Ng <sarah.ng26@gmail.com>
2021-09-03 08:59:08 -0700

    added logger middleware

--- app.js
+++ app.js
@@ -3,24 +3,35 @@ const app = express();
 const { users } = require('./database');
 const port = 5000;
 
-app.get('/', (req, res) => {
+/** Logger Middleware 
+ * - Express will supply req, res, next into middleware
+ * - next() -  passes on to the next middleware
+ * - either send its response or next()
+*/
+const logger = (req, res, next) => {
+  const { method, url } = req;
+  console.log(method, url, new Date().getFullYear());
+  next();
+}
+
+app.get('/', logger, (req, res) => {
   console.log(`${req.method} ${req.url}`);
   res.json();
 })
 
-app.get('/api/users/:userId', (req, res) => {
+app.get('/api/users/:userId', logger, (req, res) => {
   const { userId } = req.params;
   const userData = users.find((user) => user.id === userId);
   return userData ? res.json(userData) : res.status(404).send('Cannot find user');
 })
 
-app.get('/api/users/:userId/sections', (req, res) => {
+app.get('/api/users/:userId/sections', logger, (req, res) => {
   const { userId } = req.params;
   const sections = users.find((user) => user.id === userId).sections;
   return sections ? res.json(sections) : res.status(404).send('Cannot find sections');
 })
 
-app.get('/api/users/:userId', (req, res) => {
+app.get('/api/users/:userId', logger, (req, res) => {
   const { userId } = req.params;
   const { sectionId } = req.query;
   const section = users.find(user => user.id === userId)
@@ -29,7 +40,7 @@ app.get('/api/users/:userId', (req, res) => {
 })
 
 /** covers all methods at all paths */
-app.all('*', (req, res) => {
+app.all('*', logger, (req, res) => {
   console.log(`${req.method} ${req.url}`);
   return res.status(404).send('<h1>Page not found :(</h1>');
 })
