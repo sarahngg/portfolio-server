bd22d16841f23d10ce9ea58772dc06352787273b
Sarah Ng <sarah.ng26@gmail.com>
2021-09-03 08:50:11 -0700

    handled request query string and route params

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 const express = require('express');
 const app = express();
+const { users } = require('./database');
 const port = 5000;
 
 app.get('/', (req, res) => {
   console.log(`${req.method} ${req.url}`);
-  res.status(200).send('<h1>Home</h1><div>Using Express</div>');
+  res.json();
 })
 
-app.get('/contact', (req, res) => {
-  console.log(`${req.method} ${req.url}`);
-  /** can also rely on express to set status */
-  res.send('<h1>Contact</h1><div>Using Express</div>');
+app.get('/api/users/:userId', (req, res) => {
+  const { userId } = req.params;
+  const userData = users.find((user) => user.id === userId);
+  return userData ? res.json(userData) : res.status(404).send('Cannot find user');
+})
+
+app.get('/api/users/:userId/sections', (req, res) => {
+  const { userId } = req.params;
+  const sections = users.find((user) => user.id === userId).sections;
+  return sections ? res.json(sections) : res.status(404).send('Cannot find sections');
+})
+
+app.get('/api/users/:userId', (req, res) => {
+  const { userId } = req.params;
+  const { sectionId } = req.query;
+  const section = users.find(user => user.id === userId)
+  .sections.find(section => section.id === sectionId);
+  return section ? res.json(section) : res.status(404).send('Cannot find section');
 })
 
 /** covers all methods at all paths */
 app.all('*', (req, res) => {
   console.log(`${req.method} ${req.url}`);
-  res.status(404).send('<h1>Page not found :(</h1>');
+  return res.status(404).send('<h1>Page not found :(</h1>');
 })
 
 app.listen(port, () => {
